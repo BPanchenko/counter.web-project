c7d804f220392dcb06a568148689d42712e80132
Boris Panchenko <oberon@list.ru>
2022-05-09 16:21:31 +0300

    privacy

--- files/db_connect.php
+++ files/db_connect.php
@@ -1,10 +1,10 @@
 <?
 @mysql_close();
 
-define("_DB_CONNECT_DATA_HOSTNAME", "bp.mysql");
-define("_DB_CONNECT_DATA_USERNAME", "bp_mysql");
-define("_DB_CONNECT_DATA_PSW", "dgy9dzuj");
-define("_DB_CONNECT_DATA_DBNAME", "bp_protosite");
+define("_DB_CONNECT_DATA_HOSTNAME", "******");
+define("_DB_CONNECT_DATA_USERNAME", "******");
+define("_DB_CONNECT_DATA_PSW", "******");
+define("_DB_CONNECT_DATA_DBNAME", "******");
 
 mysql_connect(_DB_CONNECT_DATA_HOSTNAME,_DB_CONNECT_DATA_USERNAME,_DB_CONNECT_DATA_PSW)
 			or die("Не могу создать соединение. Ответ сервера: ".mysql_error());
